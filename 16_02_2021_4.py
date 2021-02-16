83c7f393867c653fb7e88de6725db2e1c70bfc46
Annstasia <45208486+Annstasia@users.noreply.github.com>
2021-02-16 11:33:37 +0300

    Add files via upload

---
+++ 16_02_2021_4.py
@@ -0,0 +1,16 @@
+for i in range(190061, 190072):
+    n = 0
+    dels = []
+    for j in range(1, i+1, 2):
+        if i % j == 0:
+            n += 1
+            dels.append(j)
+            if n > 4:
+                break
+    if n == 4:
+        print(i, dels[-1], dels[-2])
+
+# Ответ:
+# 190061 190061 6131
+# 190064 11879 1697
+# 190067 190067 2677
